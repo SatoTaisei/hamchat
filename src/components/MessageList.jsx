bfd79ed13977efb129e4917a3eb504be1b91e7a7
SatoTaisei <t.sei43rm@gmail.com>
2021-12-05 20:57:49 +0900

    🎨 メッセージ一覧の取得

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@mui/styles";
+import { query, limitToLast, orderByKey, onValue } from "firebase/database";
+
+import { messagesRef } from "src/firebase";
 
 const useStyles = makeStyles({
   root: {
@@ -9,5 +12,26 @@ const useStyles = makeStyles({
 
 export const MessageList = () => {
   const classes = useStyles();
+  const [messages, setMessages] = useState([]);
+
+  const messageQuery = query(
+    messagesRef,
+    orderByKey("message"),
+    limitToLast(3)
+  );
+  useEffect(() => {
+    onValue(messageQuery, (snapshot) => {
+      const messages = snapshot.val();
+      if (messages === null) return;
+
+      const entries = Object.entries(messages);
+      const newMessages = entries.map((entry) => {
+        const [key, nameAndText] = entry;
+        return { key, ...nameAndText };
+      });
+      setMessages(newMessages);
+    });
+  }, []);
+
   return <div className={classes.root}>MessageList</div>;
 };
